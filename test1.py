1530a8e61978969ab1eab561eb5b9d0a4befeaf8
Mariam <mariam.mourad@student.uni-pr.edu>
2023-04-14 13:43:10 +0200

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,18 @@
+import base64
+
+def base32_encode(data):
+    if isinstance(data, str):
+        data = data.encode()
+    return base64.b32encode(data).decode(data)
+
+def base32_decode(data):
+    if isinstance(data, str):
+        data = data.encode()
+    return base64.b32decode(data).decode(data)
+
+text = "Hello, world!"
+encoded_text = base32_encode(text)
+print(encoded_text)
+
+decoded_text = base32_decode(encoded_text)
+print(decoded_text)
