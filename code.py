7655b7df37b64c4d09f64840a9008926f9d4012e
Mariam Mourad <108888673+mariam1mourad@users.noreply.github.com>
2023-04-15 00:25:12 +0200

    Update code.py
    base32_decode function is similar to the base32_encode function but performs the reverse operation

--- code.py
+++ code.py
@@ -4,3 +4,8 @@ def base32_encode(data):
     if isinstance(data, str):
         data = data.encode()
     return base64.b32encode(data).decode()
+    
+ def base32_decode(data):
+    if isinstance(data, str):
+        data = data.encode()
+    return base64.b32decode(data).decode()
