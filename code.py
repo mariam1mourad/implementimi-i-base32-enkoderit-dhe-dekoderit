ab0f1d027547d62fb5bdde938f6d4eabfb45164f
MarigonaHamiti <115891512+marigonahamiti@users.noreply.github.com>
2023-04-15 00:34:49 +0200

    Update code.py

--- code.py
+++ code.py
@@ -9,3 +9,8 @@ def base32_encode(data):
     if isinstance(data, str):
         data = data.encode()
     return base64.b32decode(data).decode()
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+encoded_alphabet = base32_encode(alphabet)
+print(encoded_alphabet)  
+
