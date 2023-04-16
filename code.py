b4b73a897c0b862d5d36b630da657dc21fb92d5f
Mariam Mourad <108888673+mariam1mourad@users.noreply.github.com>
2023-04-16 12:29:32 +0200

    Update code.py

--- code.py
+++ code.py
@@ -18,4 +18,18 @@ print('Encoded text:',encoded_text)
 decoded_text = base32_decode(encoded_text)
 print('Decoded text:', decoded_text )
 
+print('--------------------Encoded & Decoded numbers--------------------')
+numbers = "1234567890"
+encoded_numbers = base32_encode(numbers)
+print('Encoded numbers:',encoded_numbers)
 
+decoded_numbers = base32_decode(encoded_numbers)
+print('Decoded numbers:',decoded_numbers)
+
+print('--------------------Encoded & Decoded symbols--------------------')
+symbols = "!@#$%^&*()_+,./"
+encoded_symbols = base32_encode(symbols)
+print('Encoded symbols:',encoded_symbols)
+
+decoded_symbols = base32_decode(encoded_symbols)
+print('Decoded symbols:',decoded_symbols)
